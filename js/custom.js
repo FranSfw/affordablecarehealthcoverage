db7f9381c2363d13788a206a12019d55a618aa7a
FranSfw <theweatherreport33@gmail.com>
2024-02-28 21:19:03 -0700

    Falta el js

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,10 @@
 var year = new Date().getFullYear();
 document.getElementById("year").innerHTML = year;
+
+var btnYesR1 = document.getElementById("btnYesR1");
+var btnNoR1 = document.getElementById("btnNoR1");
+var btnYesR2 = document.getElementById("btnYesR2");
+var btnNoR2 = document.getElementById("btnNoR2");
+var btnR3 = document.getElementById("btnYesR3");
+
+
